1ba096a88dabb278fde38a95331c3a44753b222f
Durgesh Mali <durgeshmali5824@gmail.com>
2025-09-04 07:54:53 +0530

    made better UI and UX

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.pdfmanagement"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.pdfmanagement"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
